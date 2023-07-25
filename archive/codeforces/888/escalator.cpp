397bb6d8bba604f78bc3e010f8f9622e50bf226f
enwask <er742165@ucf.edu>
2023-07-25 11:53:57 -0500

    Add CF 888 solutions

---
+++ archive/codeforces/888/escalator.cpp
@@ -0,0 +1,65 @@
+// settings
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#pragma ide diagnostic ignored "OCUnusedMacroInspection"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// primitive types
+typedef long long ll;
+typedef long int li;
+
+// aliases
+#define vec vector
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+typedef vec<int> vi;
+typedef vec<li> vli;
+typedef vec<ll> vll;
+typedef vec<pi> vpi;
+typedef vec<string> vs;
+
+// more aliases
+#define pb push_back
+#define eb emplace_back
+
+// other macros
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rev(i, a, b) for (int i = a; i > b; i--)
+#define travis(x, s) for (auto &x: s)
+#define integral(return_type) template<typename T> typename enable_if<is_integral<T>::value, return_type>::type
+#define numeric(return_type) template<typename T> typename enable_if<is_arithmetic<T>::value, return_type>::type
+
+// templates
+// @formatter:off
+template<typename A, typename B> ostream &operator<<(ostream &stream, const pair<A, B> &pair) { return stream << "(" << pair.first << ", " << pair.second << ")"; }
+// @formatter:on
+
+void solve() {
+    int n, m, k, h, p;
+    cin >> n >> m >> k >> h;
+
+    int num = 0;
+    rep(i, 0, n) {
+        cin >> p;
+        int dif = abs(h - p);
+        if (dif % k == 0 && dif / k < m && dif / k > 0) {
+            num++;
+        }
+    }
+
+    cout << num << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false); // makes io faster, desyncs c-style io (no scanf/printf)
+    cin.tie(nullptr); // unties cin from cout (don't do this if you alternate input/output)
+
+    int tests;
+    cin >> tests;
+    while (tests--) solve();
+}
