bfb554a95efa9a0cf1b3e9f3ec4292bacf07ffbb
enwask <er742165@ucf.edu>
2023-07-29 12:00:13 -0500

    Update main from template

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// settings
+// Settings
 #pragma GCC optimize("O3")
 #pragma GCC optimize("unroll-loops")
 #pragma ide diagnostic ignored "OCUnusedMacroInspection"
@@ -7,12 +7,16 @@
 
 using namespace std;
 
-// primitive types
-typedef long long ll;
-typedef long int li;
-
-// aliases
+// Few word type fast zoom
 #define vec vector
+#define pb push_back
+#define eb emplace_back
+
+// Type aliases
+typedef long int li;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned char byte;
 typedef pair<int, int> pi;
 typedef pair<ll, ll> pll;
 typedef vec<int> vi;
@@ -21,11 +25,7 @@ typedef vec<ll> vll;
 typedef vec<pi> vpi;
 typedef vec<string> vs;
 
-// more aliases
-#define pb push_back
-#define eb emplace_back
-
-// other macros
+// Other macros
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
 #define rep(i, a, b) for (int i = a; i < b; i++)
@@ -34,9 +34,12 @@ typedef vec<string> vs;
 #define integral(return_type) template<typename T> typename enable_if<is_integral<T>::value, return_type>::type
 #define numeric(return_type) template<typename T> typename enable_if<is_arithmetic<T>::value, return_type>::type
 
-// templates
+// Misc templates
 // @formatter:off
+template<typename A, typename B> istream &operator>>(istream &stream, pair<A, B> &pair) { stream >> pair.first >> pair.second; return stream; }
 template<typename A, typename B> ostream &operator<<(ostream &stream, const pair<A, B> &pair) { return stream << "(" << pair.first << ", " << pair.second << ")"; }
+template<typename T> istream &operator>>(istream &stream, vector<T> &vector) { for (auto &x: vector) cin >> x; return stream; }
+template<typename T> istream &operator>>(istream &stream, T array[]) { for (auto &x: array) cin >> x; return stream; }
 // @formatter:on
 
 void solve() {
@@ -44,8 +47,8 @@ void solve() {
 }
 
 int main() {
-    ios::sync_with_stdio(false); // makes io faster, desyncs c-style io (no scanf/printf)
-//    cin.tie(nullptr); // unties cin from cout (don't do this if you alternate input/output)
+    ios::sync_with_stdio(false); // Makes io faster, desyncs c-style io (no scanf/printf)
+    //cin.tie(nullptr); // Unties cin from cout (don't do this if you alternate input/output)
 
     int tests;
     cin >> tests;
