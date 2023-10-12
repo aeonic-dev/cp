cce26e2a0a0eeadd73da7dfb7df6f747f0b8c5f6
enwask <er742165@ucf.edu>
2023-10-12 17:32:59 -0400

    Generify bits, add count_bits_set

--- hackpack/bits.cpp
+++ hackpack/bits.cpp
@@ -1,17 +1,33 @@
-void unset_bit(ll &mask, int i) {
+template<typename n_t>
+void unset_bit(n_t &mask, int i) {
     mask &= ~(1 << i);
 }
 
-void set_bit(ll &mask, int i, bool val = true) {
+template<typename n_t>
+void set_bit(n_t &mask, int i, bool val = true) {
     if (val) mask |= (1 << i);
     else unset_bit(mask, i);
 }
 
-bool get_bit(ll &mask, int i) {
+template<typename n_t>
+bool get_bit(n_t &mask, int i) {
     return (mask >> i) & 1;
 }
 
-bool flip_bit(ll &mask, int i) {
+template<typename n_t>
+bool flip_bit(n_t &mask, int i) {
     mask ^= (1 << i);
     return get_bit(mask, i);
+}
+
+// Counts bits via Kernighan's algorithm
+// https://graphics.stanford.edu/~seander/bithacks.html#CountBitsSetKernighan
+template<typename n_t>
+int count_bits_set(n_t mask) {
+    int count;
+    for (count = 0; mask; count++) {
+        mask &= mask - 1;
+    }
+
+    return count;
 }
