ba45f1eb4e7335670b0c986d10b33155cd21aa5f
enwask <er742165@ucf.edu>
2023-11-06 23:49:43 -0500

    Switch to kactl template

--- main.cpp
+++ main.cpp
@@ -1,69 +1,26 @@
-// Settings
-#pragma GCC optimize("O3")
-#pragma GCC optimize("unroll-loops")
-#pragma ide diagnostic ignored "OCUnusedMacroInspection"
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
-// Few word type fast zoom
-#define vec vector
-#define pb push_back
-#define eb emplace_back
-#define uset unordered_set
-#define umap unordered_map
-
-// Type aliases
-typedef long int li;
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
 typedef long long ll;
-typedef long double ld;
-typedef unsigned char byte;
-typedef pair<int, int> pi;
-typedef pair<double, double> pd;
-typedef pair<li, li> pli;
-typedef pair<ll, ll> pll;
-typedef pair<ld, ld> pld;
-typedef vec<int> vi;
-typedef vec<li> vli;
-typedef vec<ll> vll;
-typedef vec<pi> vpi;
-typedef vec<string> vs;
-
-// Other macros
-#define all(x) x.begin(), x.end()
-#define rall(x) x.rbegin(), x.rend()
-#define rep(i, a, b) for (int i = a; i < b; i++)
-#define rev(i, a, b) for (int i = a; i > b; i--)
-#define travis(x, s) for (auto &x: s)
-#define integral(return_type) template<typename T> typename enable_if<is_integral<T>::value, return_type>::type
-#define numeric(return_type) template<typename T> typename enable_if<is_arithmetic<T>::value, return_type>::type
-#define gin(type) get_cin<type>()
-
-// Misc templates
-// @formatter:off
-template<typename A, typename B> istream &operator>>(istream &stream, pair<A, B> &pair) { stream >> pair.first >> pair.second; return stream; }
-template<typename A, typename B> ostream &operator<<(ostream &stream, const pair<A, B> &pair) { return stream << pair.first << " " << pair.second; }
-template<typename T> istream &operator>>(istream &stream, vector<T> &vector) { for (auto &x: vector) cin >> x; return stream; }
-template<typename T> istream &operator>>(istream &stream, T array[]) { for (auto &x: array) cin >> x; return stream; }
-template<typename T> class ipq : public priority_queue<T, vector<T>, greater<T>> {};
-template<typename T> T get_cin() { T t; cin >> t; return t; }
-// @formatter:on
+typedef pair<int, int> pii;
+typedef vector<int> vi;
 
 void solve() {
 
 }
 
-#define MULTIPLE_CASES 1
 int main() {
-    ios::sync_with_stdio(false); // Makes io faster, desyncs c-style io (no scanf/printf)
-    //cin.tie(nullptr); // Unties cin from cout (don't do this if you alternate input/output)
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
 
-#if MULTIPLE_CASES
-    int tests;
-    cin >> tests;
-    while (tests--) solve();
-#else
+    int t = 1;
+//    cin >> t; // Uncomment for single test case
+    while (t--) solve();
     solve();
-#endif
+
+    return 0;
 }
