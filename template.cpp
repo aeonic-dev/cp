3ad3a997220950c1096ffdc96032200a668c9bb5
enwask <er742165@ucf.edu>
2023-12-17 23:10:26 -0600

    Clean up template

--- template.cpp
+++ template.cpp
@@ -4,12 +4,9 @@ using namespace std;
 
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
 #define all(x) begin(x), end(x)
-#define sz(x) (int)(x).size()
 #define vec vector
 typedef long long ll;
 typedef long double ld;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
 
 void solve() {
 }
