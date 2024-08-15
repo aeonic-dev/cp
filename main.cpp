2a248fae219365191a803b0fb5278bb2bb43e6f2
enwask <er742165@ucf.edu>
2024-08-15 13:14:55 -0500

    Refresh template

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
-#define vec vector
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
 #define all(x) begin(x), end(x)
+#define sz(x) (int) (x).size()
 typedef long long ll;
+typedef vector<int> vi;
 
 void solve() {
+    
 }
 
 int main() {
@@ -14,7 +16,7 @@ int main() {
     cin.exceptions(cin.failbit);
 
     int t = 1;
-//    cin >> t;
+    // cin >> t;
     while (t--) solve();
 
     return 0;
--- template.cpp
+++ template.cpp
@@ -1,22 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
 #define all(x) begin(x), end(x)
-#define sz(x) (int)(x).size()
+#define sz(x) (int) (x).size()
 typedef long long ll;
 typedef vector<int> vi;
 
 void solve() {
+    
 }
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
-    
+
     int t = 1;
-//    cin >> t;
+    // cin >> t;
     while (t--) solve();
-    
+
     return 0;
 }
